3a039cb4629f7e536109b62e69325311a33fc25d
Thitinae <128833417+Thitinae@users.noreply.github.com>
2023-04-25 22:31:34 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 /**
- * main - A c program that print a line to the standard error
- * Return 1 (success)
+ * main - main block
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdou);
-return (1);
-}
+int s;
 
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
+}
